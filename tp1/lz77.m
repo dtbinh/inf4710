f2447af25224566e4c3109ed2fb0ca50c66e96bc
gabriel <gabriel.couvrette@gmail.com>
2013-09-11 18:26:21 -0400

    avancement de l<algo.

--- tp1/lz77.m
+++ tp1/lz77.m
@@ -10,6 +10,8 @@ function code=lz77 (signal, tailleFG, tailleDict)
     fenetreDroite = signal(1:tailleFG-tailleDict);
     fenetre = [dict fenetreDroite];
         
+    code = [];
+    
     %index sur le signal quon compresse
     i=1;
     while(i<=size(signal,2))
@@ -29,12 +31,38 @@ function code=lz77 (signal, tailleFG, tailleDict)
                     if(dict(iD) == signal(i+(jD-iD)))
                         % PAS FINI, RENDU ICI! D'AILLEUR, LA CONDITION DANS
                         % LE IF LAISSE A DESIRER.
+                        jD = jD + 1;
                     else
                         break;
                     end
                 end
+                
+                %3: si cette longueur est mieux, on en prend note.
+                cetteLongueur = jD - iD;
+                if(cetteLongueur>=meilleurLongueur)
+                    meilleurLongueur = cetteLongueur;
+                    meilleurIndexDict = iD;
+                end
             end
         end
+        % on a trouve la plus longue sequence.
+        % si aucune a ete trouvee, (mailleurLongueur = 0),
+        if(meilleurLongueur == 0)
+            % on ajoute le triplet (0, 0, valeur) a la sortie.
+            triplet = [0 0 signal(i)];
+        else
+            % sinon, on ajoute le triplet suivant: 
+            %index 1: combien de symboles derriere est-ce quon a trouve la
+            %suite?
+            triplet(1) = tailleDict - meilleurIndexDict;
+            %quelle est sa longueur?
+            triplet(2) = meilleurLongueur;
+            %quel symbole stoppe la suite?
+            triplet(3) = #$#$#$#$#$;
+        end
+        
+        
+        code = [code triplet];
         i=i+1;
     end
 end
