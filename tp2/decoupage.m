5d3957558847325ced3018051962ef5aa227278c
gabriel <gabriel.couvrette@gmail.com>
2013-09-19 19:44:18 -0400

    ajout de la fonction decoupage

---
+++ tp2/decoupage.m
@@ -0,0 +1,15 @@
+function [blocs, m, n] = decoupage(image)
+    [m n] = size(image);
+    blocs = zeros(8,8,m/8*n/8);
+    i_bloc=1;
+    for i=1:m/8
+        for j=1:n/8
+            zone = image((i-1)*8+1:i*8, (j-1)*8+1:j*8);
+            % coder ainsi pour eviter de recopier la matrice a chaque fois
+            % (plus rapide que blocs = [blocs ; zone]; puisqu'on connais
+            % deja le nombre de blocs final.
+            blocs(:,:,i_bloc) = zone;
+            i_bloc = i_bloc + 1;
+        end
+    end
+end
