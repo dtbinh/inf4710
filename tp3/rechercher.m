421c6343ad5a9db7b3f510cba2c80140330f6075
gabriel <gabriel.couvrette@gmail.com>
2013-11-08 12:44:35 -0500

    Le code pour rechercher est termine.

--- tp3/rechercher.m
+++ tp3/rechercher.m
@@ -11,3 +11,50 @@ function listedist = rechercher(nomImage,nomRepertoire)
 % 2 champs: le nom de l'image et la distance calculée.
 %****************************************************************
 % À compléter
+
+refImg = imread(nomImage);
+refHisto = genererHistogrammePondere(refImg, 4);
+listedist = [];
+
+files = dir(nomRepertoire);
+    for f = files'
+        if(isImage(f.name))
+            path = buildPath(f.name, nomRepertoire);
+            h = genererHistogrammePondere(imread(path), 4);
+            d = calculerDistanceEuclidienne(refHisto, h);
+            listedist = insertInList(listedist, makeStruct(f.name,d));
+        end
+    end
+end
+
+
+function isImage = isImage(filename)
+    if(length(filename)<4)
+        isImage=false;
+        return;
+    end
+    
+    imagesExt = ['jpg', 'png', 'bmp', 'gif'];
+    ext = filename(end-3+1:end);
+    isImage = ismember(ext, imagesExt);
+end
+
+function path = buildPath(filename, dir)
+    path = strcat(dir,'\',filename);
+end
+
+function list = insertInList(list, item)
+    
+    for i=1:length(list)
+        if list(i).distance>=item.distance
+            list = [list(1:i-1) item list(i:end)];
+            return;
+        end
+    end
+    
+    list = [list item];
+end
+
+function s = makeStruct(filename, distance)
+    s = struct('nom', filename, 'distance', distance);
+end
